3f064c798bba4567630395af27d9c25383fcdf84
Hiro <s4moozy@gmail.com>
2021-02-10 16:56:52 +0900

    Added coach registration form

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,3 +1,14 @@
 export default {
+  registerCoach(context, formData) {
+    const coachData = {
+      id: context.rootGetters.userId,
+      firstName: formData.first,
+      lastName: formData.last,
+      description: formData.desc,
+      hourlyRate: formData.rate,
+      areas: formData.areas
+    }
 
+    context.commit('registerCoach', coachData)
+  }
 }
--- src/store/modules/coaches/getters.js
+++ src/store/modules/coaches/getters.js
@@ -4,5 +4,11 @@ export default {
   },
   hasCoaches(state) {
     return state.coaches && state.coaches.length > 0
+  },
+  // Check to verify whether the user is a registered coach or not
+  isCoach(_, getters, _2, rootGetters) {
+    const coaches = getters.coaches
+    const userId = rootGetters.userId
+    return coaches.some(coach => coach.id === userId)
   }
 }
